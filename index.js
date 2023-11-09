ed9a4b12b427325eef8c19a151b3380d563a9c94
lechiffre4 <toxcoa@gmail.com>
2023-11-09 01:25:45 -0500

    "/" route setup

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 import express from "express";
 import fetch from "node-fetch";
-import 'dotenv/config'
-
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  return res.status(200).json({
+    title: "Bienvenue",
+    message: "Bienvenue sur mon serveur proxy de météo canada ! ",
+  });
+});
+
 app.get("/weather/:type", async (req, res) => {
   const { type } = req.params;
   const apiUrl = `https://meteo.gc.ca/api/app/fr/Location/QC-147?type=${type}`;
-  
+
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
     res.json(data);
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ error: "Erreur lors de la récupération des données météo" });
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des données météo" });
   }
 });
 
